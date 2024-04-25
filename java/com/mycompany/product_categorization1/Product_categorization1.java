1b32f49e7dbf53f29f97f6c0c5f829fb5a5844a0
Rompheu <rompher2609@gmail.com>
2024-04-26 00:41:12 +0700

    Product Category
    VOEUN Rompheu

---
+++ java/com/mycompany/product_categorization1/Product_categorization1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.product_categorization1;
+
+/**
+ *
+ * @author ACER
+ */
+public class Product_categorization1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
